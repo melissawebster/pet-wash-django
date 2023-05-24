8b26d4fe0a0611d93acd8e43e48e8b0922955a34
Melissa Webster <imelissawebster@gmail.com>
2023-05-23 21:11:48 -0300

    update form

--- base/views.py
+++ base/views.py
@@ -1,18 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from base.forms import ContactForm
 
+#sempre que se quer criar uma pagina em branco, tem que vir nas views
 
 def index(request):
     return render(request, 'index.html')
 
 def contact(request):
-    context = {
-        'phonenumber': '999999',
-        'tutor': 'Maria'
+    success = False
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            success= True
+
+    responsible = {
+        'responsible_number': '999999',
+        'responsible_name': 'Maria',
+        'form': form,
+        'success': success
     }
+    
     if request.method == 'POST':
         print(request.POST)
-    return render(request, 'contact.html', context)
+    return render(request, 'contact.html', responsible)
 
 
 
